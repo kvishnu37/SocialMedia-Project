c0786c659904452ba39e23672454d594d281d6d1
kvishnu37 <koripalavishnu@gmail.com>
2025-07-21 00:28:10 +0530

    updated the code in ConnectionUtil Class

--- src/main/java/Util/ConnectionUtil.java
+++ src/main/java/Util/ConnectionUtil.java
@@ -1,7 +1,7 @@
 package Util;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,61 +17,6 @@ import java.sql.SQLException;
  * DO NOT CHANGE ANYTHING IN THIS CLASS
  */
 public class ConnectionUtil {
- 
-	// /**
-	//  * url will represent our connection string. Since this is an in-memory db, we
-	//  * will represent a file location to store the data
-	//  */
-	// private static String url = "jdbc:h2:./h2/db;";
-	// /**
-	//  * Default username for connecting to h2
-	//  */
-	// private static String username = "sa";
-	// /**
-	//  * Default password for connecting to h2
-	//  */
-	// private static String password = "sa";
-
-	// /**
-	//  * DataSource for pooling. Pooling enables the creation of multiple connections when connections are closed.
-	//  */
-	// private static JdbcDataSource pool = new JdbcDataSource();
-
-	// /**
-	//  * static initialization block to establish credentials for DataSoure Pool
-	//  */
-	// static {
-	// 	pool.setURL(url);
-	// 	pool.setUser(username);
-	// 	pool.setPassword(password);
-	// }
-
-	// /**
-	//  * @return an active connection to the database
-	//  */
-	// public static Connection getConnection() {
-	// 	try {
-	// 		return pool.getConnection();
-	// 	} catch (SQLException e) {
-	// 		e.printStackTrace();
-	// 	}
-
-	// 	return null;
-	// }
-
-	// /**
-	//  * For the purpose of testing, we will need to drop and recreate our database
-	//  * tables to keep it consistent across all tests. The method will read the sql
-	//  * file in resources. This will be performed before every test.
-	//  */
-	// public static void resetTestDatabase() {
-	// 	try {
-	// 		FileReader sqlReader = new FileReader("src/main/resources/SocialMedia.sql");
-	// 		RunScript.execute(getConnection(), sqlReader);
-	// 	} catch (SQLException | FileNotFoundException e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
 	 private static String url = "jdbc:mysql://localhost:3306/socialmedia";
     private static String username = "root"; // your MySQL username
     private static String password = "Vishnu@576"; // your MySQL password
